793324c571c68367901125d83a9d076835193f08
Emmanuel Sanchez <fede@fede.website>
2021-11-11 17:08:35 -0600

    gray scale

--- MonedasContorno/contorno.py
+++ MonedasContorno/contorno.py
@@ -1,6 +1,11 @@
 # pip install opencv-contrib-python
 from cv2 import cv2
 imagen=cv2.imread('contorno.jpg')
-cv2.imshow('imagen', imagen)
+grises=cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+umbral=cv2.threshold(grises, 100, 255, cv2.THRESH_BINARY)
+
+# Mostrar
+cv2.imshow('imagen original', imagen)
+cv2.imshow('imagen en escala de grises', grises)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
