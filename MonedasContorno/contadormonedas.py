9e6d5949815b668bcf81fdb91b873a73305fa263
Emmanuel Sanchez <fede@fede.website>
2021-11-30 17:41:45 -0600

    using the gaus function

--- MonedasContorno/contadormonedas.py
+++ MonedasContorno/contadormonedas.py
@@ -2,10 +2,16 @@ from cv2 import cv2
 
 import numpy as np
 
+valorGauss = 3
+valorKernel = 3
+
 original = cv2.imread('monedas.jpg')
 gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+
+gauss = cv2.GaussianBlur(gris, (valorGauss, valorKernel), 0)
 
 #Mostrar resultado
 
 cv2.imshow('Grises', gris)
+cv2.imshow('Gaussian', gauss)
 cv2.waitKey(0)
