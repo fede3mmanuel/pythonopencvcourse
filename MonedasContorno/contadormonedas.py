c69546dc7157e2b4ff4fc08ba933c20a672cbac9
Emmanuel Sanchez <fede@fede.website>
2021-12-06 14:17:04 -0600

    removing noise, it works on monedas.jpg

--- MonedasContorno/contadormonedas.py
+++ MonedasContorno/contadormonedas.py
@@ -12,9 +12,19 @@ gauss = cv2.GaussianBlur(gris, (valorGauss, valorGauss), 0)
 
 canny = cv2.Canny(gauss, 60, 100)
 
+kernel = np.ones((valorKernel, valorKernel), np.uint8)
+cierre = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+
+contornos, jerarquia = cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print("Monedas encontradas: {}".format(len(contornos)))
+
+cv2.drawContours(original, contornos, -1, (0,0,255),2)
 #Mostrar resultado
 
 cv2.imshow('Grises', gris)
 cv2.imshow('Gaussian', gauss)
 cv2.imshow('Canny', canny)
+cv2.imshow('Cierre', cierre)
+cv2.imshow('Resultado', original)
 cv2.waitKey(0)
